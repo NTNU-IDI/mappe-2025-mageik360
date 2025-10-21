2d1936152710fed2b69750de4d43f812361aa7eb
Magnus Eik <mageik@stud.ntnu.no>
2025-10-21 13:00:17 +0200

    feat: add constructor to DiaryEntry.java - add constructor - add non-null validation to constructor

--- src/main/java/no/ntnu/dagbok/DiaryEntry.java
+++ src/main/java/no/ntnu/dagbok/DiaryEntry.java
@@ -1,4 +1,5 @@
 package no.ntnu.dagbok;
+import java.util.Objects;
 
 import java.time.LocalDateTime;
 
@@ -7,6 +8,28 @@ public class DiaryEntry {
   private String title;
   private String text;
   private LocalDateTime dateTime;
+
+  /**
+   * Constructor for a new DiaryEntry
+   *
+   * @param author The author of the diary entry. Non-null.
+   * @param title The title of the diary entry. Non-null.
+   * @param text The text of the diary entry. Non-null.
+   * @param dateTime The date and time of the diary entry. Non-null value.
+   *
+   */
+  public DiaryEntry(String author, String title, String text, LocalDateTime dateTime){
+    Objects.requireNonNull(author, "Must have author");
+    Objects.requireNonNull(title, "Must have title");
+    Objects.requireNonNull(text, "Must have text");
+    Objects.requireNonNull(text, "Must have date and time");
+
+    this.author = author;
+    this.title = title;
+    this.text = text;
+    this.dateTime = dateTime;
+
+  }
 
   public String getAuthor() { return author; }
   public String getTitle() { return title; }
