b7be060d7d4a0fb7a8a31e6bfefb922716a7cb73
Magnus Eik <mageik@stud.ntnu.no>
2025-10-21 12:51:49 +0200

    feat: add getters to DiaryEntry.java - add author getter - add title getter - add text getter - add date getter

--- src/main/java/no/ntnu/dagbok/DiaryEntry.java
+++ src/main/java/no/ntnu/dagbok/DiaryEntry.java
@@ -6,5 +6,11 @@ public class DiaryEntry {
   private String author;
   private String title;
   private String text;
-  private LocalDateTime date;
+  private LocalDateTime dateTime;
+
+  public String getAuthor() { return author; }
+  public String getTitle() { return title; }
+  public String getText() { return text; }
+  public LocalDateTime getDateTime() {return dateTime;}
+
 }
